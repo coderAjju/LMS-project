0b7c6cdd1460edabac8f0c98f56ba09388c9cf1f
coderAjju <upadhyayajay437@gmail.com>
2025-01-16 21:17:06 +0530

    purchase route file is updated

--- server/src/routes/purchase.routes.js
+++ server/src/routes/purchase.routes.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import protectedRoute from '../middleware/protectedRoute.js';
-import { createCheckoutSession, getCourseDetailWithPurchaseStatus, stripeWebhook } from '../controllers/coursePurchase.controller.js';
+import { createCheckoutSession, getAllPurchasedCourse, getCourseDetailWithPurchaseStatus, stripeWebhook } from '../controllers/coursePurchase.controller.js';
 
 const Router = express.Router();
 
 Router.post("/checkout/create-checkout-session",protectedRoute,createCheckoutSession);
 Router.post("/webhook",express.raw({type:"application/json"}),stripeWebhook);
-Router.get("/detail-with-status",getCourseDetailWithPurchaseStatus)
+Router.get("/:courseId/detail-with-status",protectedRoute,getCourseDetailWithPurchaseStatus)
+Router.get("/getPuchasedCourse",protectedRoute,getAllPurchasedCourse)
 // Router.get(`/course/:courseId/detail-with-status`);
 
 export default Router;
